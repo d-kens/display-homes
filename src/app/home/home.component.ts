1ae01d81348ed98a677a06389beb3e8d3279ad83
d-kens <omondikens1@gmail.com>
2024-03-07 12:46:03 +0300

    services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
+
 export class HomeComponent {
 
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    // Fetch all the housing locations from Housing service and assign them to housingLocationList
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
